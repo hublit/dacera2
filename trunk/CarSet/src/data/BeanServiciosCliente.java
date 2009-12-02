963de9e4f221bbd2df0a96c9fa0253db1d09728d
cesardecruz <cesardecruz@11e615fa-ca02-11de-a077-756b9c6e95db>
2009-12-02 16:50:23 +0000

    Modificaciones

---
+++ trunk/CarSet/src/data/BeanServiciosCliente.java
@@ -0,0 +1,244 @@
+package data;
+
+/**
+ *
+ * @author Raul
+ */
+public class BeanServiciosCliente
+{
+    private String campa;
+    private String entradaCampa;
+    private String todoterreno;
+    private String furgonetas;
+    private String furgones;
+    private String industrial;
+    private String idaVuelta;
+    private String chequeo;
+    private String reacondicionamiento;
+    private String lavado;
+    private String lavadoExteriorInteror;
+    private String lavadoIntegral;
+    private String lavadoExtra;
+    private String lavadoHigienizado;
+    private String lavadoIntExt4;
+    private String lavadoIntegral4;
+    private String lavadoIntExtIndustrial;
+    private String lavadoIntIndustrial;
+    private String lavadoLimpiezaPegatinas;
+    private String lavadoLimpIntPegatinas;
+    private String itv;
+    private String preItv;
+    private String urgente;
+    private String suplemento;
+    private String repostaje;
+    private String manoObraMecanicaChapa;
+
+    public String getCampa() {
+        return campa;
+    }
+
+    public void setCampa(String campa) {
+        this.campa = campa;
+    }
+
+    public String getChequeo() {
+        return chequeo;
+    }
+
+    public void setChequeo(String chequeo) {
+        this.chequeo = chequeo;
+    }
+
+    public String getEntradaCampa() {
+        return entradaCampa;
+    }
+
+    public void setEntradaCampa(String entradaCampa) {
+        this.entradaCampa = entradaCampa;
+    }
+
+    public String getFurgones() {
+        return furgones;
+    }
+
+    public void setFurgones(String furgones) {
+        this.furgones = furgones;
+    }
+
+    public String getFurgonetas() {
+        return furgonetas;
+    }
+
+    public void setFurgonetas(String furgonetas) {
+        this.furgonetas = furgonetas;
+    }
+
+    public String getIdaVuelta() {
+        return idaVuelta;
+    }
+
+    public void setIdaVuelta(String idaVuelta) {
+        this.idaVuelta = idaVuelta;
+    }
+
+    public String getIndustrial() {
+        return industrial;
+    }
+
+    public void setIndustrial(String industrial) {
+        this.industrial = industrial;
+    }
+
+    public String getItv() {
+        return itv;
+    }
+
+    public void setItv(String itv) {
+        this.itv = itv;
+    }
+
+    public String getLavado() {
+        return lavado;
+    }
+
+    public void setLavado(String lavado) {
+        this.lavado = lavado;
+    }
+
+    public String getLavadoExteriorInteror() {
+        return lavadoExteriorInteror;
+    }
+
+    public void setLavadoExteriorInteror(String lavadoExteriorInteror) {
+        this.lavadoExteriorInteror = lavadoExteriorInteror;
+    }
+
+    public String getLavadoExtra() {
+        return lavadoExtra;
+    }
+
+    public void setLavadoExtra(String lavadoExtra) {
+        this.lavadoExtra = lavadoExtra;
+    }
+
+    public String getLavadoHigienizado() {
+        return lavadoHigienizado;
+    }
+
+    public void setLavadoHigienizado(String lavadoHigienizado) {
+        this.lavadoHigienizado = lavadoHigienizado;
+    }
+
+    public String getLavadoIntExt4() {
+        return lavadoIntExt4;
+    }
+
+    public void setLavadoIntExt4(String lavadoIntExt4) {
+        this.lavadoIntExt4 = lavadoIntExt4;
+    }
+
+    public String getLavadoIntExtIndustrial() {
+        return lavadoIntExtIndustrial;
+    }
+
+    public void setLavadoIntExtIndustrial(String lavadoIntExtIndustrial) {
+        this.lavadoIntExtIndustrial = lavadoIntExtIndustrial;
+    }
+
+    public String getLavadoIntIndustrial() {
+        return lavadoIntIndustrial;
+    }
+
+    public void setLavadoIntIndustrial(String lavadoIntIndustrial) {
+        this.lavadoIntIndustrial = lavadoIntIndustrial;
+    }
+
+    public String getLavadoIntegral() {
+        return lavadoIntegral;
+    }
+
+    public void setLavadoIntegral(String lavadoIntegral) {
+        this.lavadoIntegral = lavadoIntegral;
+    }
+
+    public String getLavadoIntegral4() {
+        return lavadoIntegral4;
+    }
+
+    public void setLavadoIntegral4(String lavadoIntegral4) {
+        this.lavadoIntegral4 = lavadoIntegral4;
+    }
+
+    public String getLavadoLimpIntPegatinas() {
+        return lavadoLimpIntPegatinas;
+    }
+
+    public void setLavadoLimpIntPegatinas(String lavadoLimpIntPegatinas) {
+        this.lavadoLimpIntPegatinas = lavadoLimpIntPegatinas;
+    }
+
+    public String getLavadoLimpiezaPegatinas() {
+        return lavadoLimpiezaPegatinas;
+    }
+
+    public void setLavadoLimpiezaPegatinas(String lavadoLimpiezaPegatinas) {
+        this.lavadoLimpiezaPegatinas = lavadoLimpiezaPegatinas;
+    }
+
+    public String getManoObraMecanicaChapa() {
+        return manoObraMecanicaChapa;
+    }
+
+    public void setManoObraMecanicaChapa(String manoObraMecanicaChapa) {
+        this.manoObraMecanicaChapa = manoObraMecanicaChapa;
+    }
+
+    public String getPreItv() {
+        return preItv;
+    }
+
+    public void setPreItv(String preItv) {
+        this.preItv = preItv;
+    }
+
+    public String getReacondicionamiento() {
+        return reacondicionamiento;
+    }
+
+    public void setReacondicionamiento(String reacondicionamiento) {
+        this.reacondicionamiento = reacondicionamiento;
+    }
+
+    public String getRepostaje() {
+        return repostaje;
+    }
+
+    public void setRepostaje(String repostaje) {
+        this.repostaje = repostaje;
+    }
+
+    public String getSuplemento() {
+        return suplemento;
+    }
+
+    public void setSuplemento(String suplemento) {
+        this.suplemento = suplemento;
+    }
+
+    public String getTodoterreno() {
+        return todoterreno;
+    }
+
+    public void setTodoterreno(String todoterreno) {
+        this.todoterreno = todoterreno;
+    }
+
+    public String getUrgente() {
+        return urgente;
+    }
+
+    public void setUrgente(String urgente) {
+        this.urgente = urgente;
+    }
+
+}
