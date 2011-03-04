41d7fa2967a79af63b5a9f281036a641f73c614a
cesardecruz <cesardecruz@11e615fa-ca02-11de-a077-756b9c6e95db>
2011-03-04 12:48:38 +0000

    Subida inicial

---
+++ CarSet/src/utils/TablaModeloTesoreria.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package utils;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author depr102
+ */
+public class TablaModeloTesoreria extends DefaultTableModel {
+
+    public boolean isCellEditable (int row, int column)
+    {
+        if (column == 12 || column == 13 || column == 14)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
