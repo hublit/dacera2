3b5ecb862f36052be866fc95d6d646206ef243eb
cesardecruz <cesardecruz@11e615fa-ca02-11de-a077-756b9c6e95db>
2013-03-24 19:12:48 +0000

    Modificaciones tesorería

--- CarSet/src/utils/TablaModeloVPedidos.java
+++ CarSet/src/utils/TablaModeloVPedidos.java
@@ -11,11 +11,11 @@ import javax.swing.table.DefaultTableModel;
  *
  * @author depr102
  */
-public class TablaModeloTesoreria extends DefaultTableModel {
+public class TablaModeloVPedidos extends DefaultTableModel {
 
     public boolean isCellEditable (int row, int column)
     {
-        if (column == 11 ||column == 12 || column == 13 || column == 14|| column == 15)
+        if (column == 0)
         {
             return true;
         }
