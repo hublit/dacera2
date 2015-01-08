1df8b9ad65a3a1c945eff17405ef96a9df2a7fe1
raulcr <raulcr@11e615fa-ca02-11de-a077-756b9c6e95db>
2015-01-08 09:55:01 +0000

    Solución de incidencias

--- CarSet/src/utils/TablaValidarArchivo.java
+++ CarSet/src/utils/TablaValidarArchivo.java
@@ -15,6 +15,13 @@ public class TablaValidarArchivo extends DefaultTableModel {
 
     public boolean isCellEditable (int row, int column)
     {
-        return true;
+        if (column == 0 ||column == 1 ||column == 2 ||column == 13 || column == 14|| column == 15|| column == 16 || column == 17)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
